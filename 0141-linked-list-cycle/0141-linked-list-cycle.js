02ed3184872d17ffb11e102877ed6f8a26de9bc8
Mayur Hanwate <70631103+mayur-hanwate@users.noreply.github.com>
2025-12-15 13:24:34 +0530

    Time: 70 ms (5.13%), Space: 60.3 MB (5.76%) - LeetHub

---
+++ 0141-linked-list-cycle/0141-linked-list-cycle.js
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function(head) {
+   
+   const hashMap = new Map();
+   
+   while(head){
+
+    if(hashMap.get(head)) return true;
+
+    hashMap.set(head, head.next);
+
+     head = head.next;
+   }
+    
+    
+    return false;
+
+};
