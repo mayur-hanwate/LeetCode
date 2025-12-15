d71d42681c1b36184c4a907864e7b54ef270d8d9
Mayur Hanwate <70631103+mayur-hanwate@users.noreply.github.com>
2025-12-15 13:36:02 +0530

    Time: 58 ms (51.45%), Space: 60.4 MB (5.76%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.js
+++ 0141-linked-list-cycle/0141-linked-list-cycle.js
@@ -12,13 +12,13 @@
  */
 var hasCycle = function(head) {
    
-   const hashMap = new Map();
+   const hashMap = new Set();
    
    while(head){
 
     if(hashMap.has(head)) return true;
 
-    hashMap.set(head, head.next);
+    hashMap.add(head, head.next);
 
     head = head.next;
    }
