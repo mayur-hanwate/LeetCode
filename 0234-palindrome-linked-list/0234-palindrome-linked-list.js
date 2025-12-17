6a2f79b12ba6efdff67007b686b2ef53fa1e361f
Mayur Hanwate <70631103+mayur-hanwate@users.noreply.github.com>
2025-12-17 13:44:56 +0530

    Time: 9 ms (45.1%), Space: 84.9 MB (21.41%) - LeetHub

---
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.js
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var isPalindrome = function(head) {
+    
+    let arr = [];
+
+    while(head){
+        arr.push(head.val);
+        head = head.next;
+    }
+
+    for(let i=0;i<arr.length/2;i++){
+        if(arr[i] !== arr[arr.length-i-1]) return false;
+    }
+
+    return true;
+};
