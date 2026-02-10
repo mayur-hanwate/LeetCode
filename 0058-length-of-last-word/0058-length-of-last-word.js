46b9b1fb149ad01138190da356f4f69816ffab65
Mayur Hanwate <70631103+mayur-hanwate@users.noreply.github.com>
2026-02-10 20:42:53 +0530

    Time: 2 ms (8.43%), Space: 53.3 MB (79.84%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.js
+++ 0058-length-of-last-word/0058-length-of-last-word.js
@@ -3,8 +3,30 @@
  * @return {number}
  */
 var lengthOfLastWord = function(s) {
-    
-    const arr = s.trim().split(' ');
 
-    return arr[arr.length-1].length;
+    let strLength = 0;
+    let isValidSpace = true;
+    
+    for(let i =0;i<s.length;i++){
+        if(isValidSpace && s[i] !== ' '){
+            strLength = 0;
+            isValidSpace = false;
+        }
+
+        if(s[i] !== ' '){
+            strLength++;
+        }else{
+            isValidSpace = true;
+        }
+    }
+
+    return strLength;
+   
 };
+
+// Traditional way
+
+// Loop through the array.
+// Look for 2 conditions toreset a string and add a string
+// 1. If i reach the end, the last string would be my length
+// 2. If i reach space and a letter after that then reset my num count
