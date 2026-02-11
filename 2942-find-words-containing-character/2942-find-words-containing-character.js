e0416a0a3034735c81858023eafae2e9a06d05f2
Mayur Hanwate <70631103+mayur-hanwate@users.noreply.github.com>
2026-02-11 21:52:07 +0530

    Time: 0 ms (100%), Space: 57.6 MB (69.18%) - LeetHub

---
+++ 2942-find-words-containing-character/2942-find-words-containing-character.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string[]} words
+ * @param {character} x
+ * @return {number[]}
+ */
+var findWordsContaining = function(words, x) {
+    
+    let count = [];
+
+    for(let i=0; i<words.length; i++){
+        if(words[i].includes(x)){
+            count.push(i);
+        }
+    }
+
+    return count;
+
+};
